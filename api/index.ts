68f08fa70435503515a63879db4cadc58e6a0352
KristianLyng99 <klyngv99@gmail.com>
2025-08-07 23:16:57 +0200

    fix: await route registration and log errors

---
+++ api/index.ts
@@ -0,0 +1,17 @@
+import express, { type Request, type Response, type NextFunction } from "express";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+await registerRoutes(app);
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.status || err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  console.error(err);
+  res.status(status).json({ message });
+});
+
+export default app;
